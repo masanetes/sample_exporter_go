5ffba328b05758a1807f4019cc1572118e900492
masanetes <masanetes@gmail.com>
2022-06-09 20:14:34 +0900

    impl custom collector

--- main.go
+++ main.go
@@ -2,31 +2,51 @@ package main
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
-	"time"
+	"runtime"
 )
 
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
-	})
-)
+type collector struct {
+	goroutinesDesc *prometheus.Desc
+	threadsDesc    *prometheus.Desc
+}
 
-func recordMetrics() {
-	for {
-		opsProcessed.Inc()
-		time.Sleep(2 * time.Second)
+func newCollector() *collector {
+	return &collector{
+		goroutinesDesc: prometheus.NewDesc(
+			"google_status",
+			"Number of goroutines that currently exist.",
+			nil, nil),
+		threadsDesc: prometheus.NewDesc(
+			"threads",
+			"Number of OS threads created.",
+			nil, nil),
 	}
 }
 
-func main() {
-	go recordMetrics()
+func (c *collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.goroutinesDesc
+	ch <- c.threadsDesc
+}
 
-	http.Handle("/metrics", promhttp.Handler())
+func (c *collector) Collect(ch chan<- prometheus.Metric) {
+
+	resp, _ := http.Get("https://google.com")
+
+	log.Println("scrape")
+
+	ch <- prometheus.MustNewConstMetric(c.goroutinesDesc, prometheus.GaugeValue, float64(resp.StatusCode))
+	n, _ := runtime.ThreadCreateProfile(nil)
+	ch <- prometheus.MustNewConstMetric(c.threadsDesc, prometheus.GaugeValue, float64(n))
+}
+
+func main() {
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(newCollector())
+
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
 	if err := http.ListenAndServe(":19115", nil); err != nil {
 		log.Fatal(err)
 	}
